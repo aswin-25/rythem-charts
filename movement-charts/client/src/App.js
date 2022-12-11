b03441de01717b8440a511125370758b38668907
Aswin_Selva <aswinselva0705@gmail.com>
2022-12-11 16:14:14 +0530

    dated dropdown updated

--- movement-charts/client/src/App.js
+++ movement-charts/client/src/App.js
@@ -27,18 +27,37 @@ ChartJS.register(
   zoomPlugin
 );
 
-const fields = ['EBL', 'EBR', 'EBT', 'NBL', 'NBR', 'NBT', 'SBL', 'SBR', 'SBT', 'WBL', 'WBR', 'WBT'];
+const shortMonth = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 const App = () => {
   const [apiData, setApiData] = useState([]);
   const [chartData, setChartData] = useState();
+  const [fieldList, setFieldList] = useState([]);
+  const [dateList, setDateList] = useState([]);
   const [selectedOption, setSelectedOption] = useState('');
+  const [selectedDate, setSelectedDated] = useState('');
+  const [interval, setInterval] = useState(900000);
 
   useEffect(() => {
-    if (selectedOption !== '') {
-      getDatabyName(selectedOption);
+    fetch(`http://localhost:4000/getOptions`).then((res) => res.json())
+      .then(data => {
+        setDateList(data.dateList);
+        setFieldList(data.fieldList);
+      })
+  }, []);
+
+  useEffect(() => {
+    if (apiData.length) {
+      console.log('called');
+      setChartFields();
     }
-  }, [selectedOption])
+  }, [interval])
+
+  useEffect(() => {
+    if (selectedOption !== '' && selectedDate !== '') {
+      getDatabyName(selectedOption, selectedDate);
+    }
+  }, [selectedOption, selectedDate]);
 
   const chartOptions = {
     responsive: true,
@@ -48,7 +67,7 @@ const App = () => {
       },
       title: {
         display: true,
-        text: `Movement data for ${selectedOption}`
+        text: `Movement data for ${selectedOption} on ${selectedDate}`
       },
       zoom: {
         pan: {
@@ -68,7 +87,16 @@ const App = () => {
     }
   };
 
-  const getDatabyName = (field) => fetch(`http://localhost:4000/getData/${field}`).then((res) => res.json())
+  const getDatabyName = (field, date) => fetch(`http://localhost:4000/getData`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      movementDate: date,
+      field: field
+    })
+  }).then((res) => res.json())
     .then(data => {
       setApiData(data);
       setChartFields(data);
@@ -78,13 +106,19 @@ const App = () => {
     const chartLabels = [];
     const chartDataSet = [];
     data.forEach(element => {
-      const entDate = parseInt(element.entry_date);
-      const date = `${new Date(entDate).getDate()}-${new Date(entDate).getMonth() + 1} - ${new Date(entDate).getHours()}:${new Date(entDate).getMinutes()}`
-      chartLabels.push(date);
-      chartDataSet.push(element.movements.Total);
+      const curDate = parseInt(element.entry_date);
+      if (interval > 900000 && (curDate - chartLabels[chartLabels.length - 1]) <= interval) {
+        chartDataSet[chartDataSet.length - 1] += element.movements.Total;
+      } else {
+        chartLabels.push(curDate);
+        chartDataSet.push(element.movements.Total);
+      }
     });
     const chart = {
-      labels: chartLabels,
+      labels: chartLabels.map(date => {
+        const entDate = new Date(date);
+        return `${entDate.getDate()} ${shortMonth[entDate.getMonth()]} - ${entDate.getHours()}:${new Date(entDate).getMinutes()}`
+      }),
       datasets: [
         {
           label: `${selectedOption} Movement Count`,
@@ -102,18 +136,63 @@ const App = () => {
       <Container>
         <Row className="justify-content-center">
           <Col sm={8} md={4} className="mb-3">
+            <Form.Label htmlFor="select-date">Select Date</Form.Label>
+            <Form.Select onChange={(e) => setSelectedDated(e.target.value)} id="select-date">
+              <option value="">--Select Date--</option>
+              {dateList.map((opt, i) => <option key={`${opt}-${i}`}>{opt}</option>)}
+            </Form.Select>
+          </Col>
+          <Col sm={8} md={4} className="mb-3">
             <Form.Label htmlFor="select-type">Select Field to Display</Form.Label>
             <Form.Select onChange={(e) => setSelectedOption(e.target.value)} id="select-type">
-              <option>--Select Type--</option>
-              {fields.map((opt, i) => <option key={`${opt}-${i}`}>{opt}</option>)}
+              <option value="">--Select Type--</option>
+              {fieldList.map((opt, i) => <option key={`${opt}-${i}`}>{opt}</option>)}
             </Form.Select>
           </Col>
           <Col sm={12}>
             {chartData ? (
               <div className="chart-area">
+                <div className="mb-3">
+                  <Form.Check
+                    inline
+                    label="15 mins"
+                    name="interval"
+                    type="radio"
+                    id="15-min"
+                    checked={interval === 900000}
+                    onChange={() => setInterval(900000)}
+                  />
+                  <Form.Check
+                    inline
+                    label="30 mins"
+                    name="interval"
+                    type="radio"
+                    id="30-min"
+                    checked={interval === 1800000}
+                    onChange={() => setInterval(1800000)}
+                  />
+                  <Form.Check
+                    inline
+                    label="1 hr"
+                    name="interval"
+                    type="radio"
+                    id="1-hr"
+                    checked={interval === 3600000}
+                    onChange={() => setInterval(3600000)}
+                  />
+                  <Form.Check
+                    inline
+                    label="2 hrs"
+                    name="interval"
+                    type="radio"
+                    id="2-hr"
+                    checked={interval === 7200000}
+                    onChange={() => setInterval(7200000)}
+                  />
+                </div>
                 <Line options={chartOptions} data={chartData} />
               </div>
-            ) : <p>Please select some data to display Chart</p>}
+            ) : <p>Please select Date and field to display Chart</p>}
           </Col>
         </Row>
       </Container>
--- movement-charts/server/index.js
+++ movement-charts/server/index.js
@@ -1,28 +1,60 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const app = express()
-const apiPort = 4000
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const apiPort = 4000;
 
+const shortMonth = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 const fs = require("fs");
 
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors());
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 app.use(bodyParser.json())
 
-
-app.get('/getData/:field', (req, res) => {
+app.post('/getData', (req, res, next) => {
 	fs.readFile("./dummyData.json", "utf8", (err, jsonString) => {
 		if (err) {
 			res.status(500).json({ error: err });
 		}
-		const jsonData = JSON.parse(jsonString)
-		const resData = jsonData.TMC.map(tot => ({
-			movements: tot.movements[`${req.params.field}`],
-			entry_date: tot.entry_date
-		}))
+		const jsonData = JSON.parse(jsonString);
+		const reqDate = new Date(req.body.movementDate);
+		const resData = jsonData.TMC
+			.filter(val => val.entry_date >= reqDate.setUTCHours(0, 0, 0, 0) && val.entry_date <= reqDate.setUTCHours(23, 59, 59, 999))
+			.map(tot => ({
+				movements: tot.movements[`${req.body.field}`],
+				entry_date: tot.entry_date
+			}));
 		res.status(200).json(resData);
 	});
-})
+});
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+app.get('/getOptions', (req, res, next) => {
+	fs.readFile("./dummyData.json", "utf8", (err, jsonString) => {
+		if (err) {
+			res.status(500).json({ error: err });
+		}
+		const dateList = [];
+		const fieldList = [];
+		const jsonData = JSON.parse(jsonString);
+		jsonData.TMC.forEach(tot => {
+			const entDate = new Date(parseInt(tot.entry_date));
+			const dateItem = `${entDate.getDate()} ${shortMonth[entDate.getMonth()]} ${entDate.getFullYear()}`;
+			Object.keys(tot.movements).forEach(field => {
+				if (!fieldList.includes(field)) {
+					fieldList.push(field);
+				}
+			});
+			if (!dateList.includes(dateItem)) {
+				dateList.push(dateItem);
+			}
+		})
+		res.status(200).json({ dateList, fieldList });
+	});
+});
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
